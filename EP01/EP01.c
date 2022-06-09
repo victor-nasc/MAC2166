229173bb3382dd75a2c79455ec9cc1488554d2ee
victor-nasc <105170852+victor-nasc@users.noreply.github.com>
2022-06-09 19:14:38 -0300

    Add files via upload

---
+++ EP01/EP01.c
@@ -0,0 +1,36 @@
+/*
+  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP, 
+  DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA. 
+  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO PROGRAMA (EP) FORAM 
+  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES
+  DESSE EP E QUE PORTANTO NÃO CONSTITUEM DESONESTIDADE ACADÊMICA
+  OU PLÁGIO.  
+  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS AS CÓPIAS
+  DESSE PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A
+  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO E
+  DESONESTIDADE ACADÊMICA SERÃO TRATADOS SEGUNDO OS CRITÉRIOS
+  DIVULGADOS NA PÁGINA DA DISCIPLINA.
+  ENTENDO QUE EPS SEM ASSINATURA NÃO SERÃO CORRIGIDOS E,
+  AINDA ASSIM, PODERÃO SER PUNIDOS POR DESONESTIDADE ACADÊMICA.
+
+  Nome :
+  NUSP :
+  Turma:
+  Prof.:
+
+  Referências: Com exceção das rotinas fornecidas no enunciado
+  e em sala de aula, caso você tenha utilizado alguma refência,
+  liste-as abaixo para que o seu programa não seja considerado
+  plágio ou irregular.
+  
+  Exemplo:
+  - O algoritmo Quicksort foi baseado em
+  http://www.ime.usp.br/~pf/algoritmos/aulas/quick.html
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    
+}
